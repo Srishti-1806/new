197ad894861331c496e86a6e8fd8d6005ce99a78
Srishti Mishra <43srishtimishra@gmail.com>
2025-07-26 01:50:32 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,13 @@
-const { createServer } = require("./server");
+import express from "express";
+import cors from "cors";
+
+export function createServer() {
+  const app = express();
+  app.use(cors());
+
+  app.get("/api", (_, res) => {
+    res.json({ message: "API is working!" });
+  });
+
+  return app;
+}
